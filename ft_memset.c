e8c27f52bb2a3da4992d6769030743e3ea10373f
Tomonari Muranaka <tmuranak@c1r3s9.42tokyo.jp>
2024-04-16 16:27:39 +0900

    add memset function

---
+++ ft_memset.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmuranak <tmuranak@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/16 15:52:07 by tmuranak          #+#    #+#             */
+/*   Updated: 2024/04/16 16:26:10 by tmuranak         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+
+void	*ft_memset(void *buf, int ch, size_t n)
+{
+	size_t	c;
+	char	*str;
+
+	c = 0;
+	str = buf;
+	while (c < n)
+	{
+		str[c] = ch;
+		c++;
+	}
+	return (buf);
+}
+
+int main()
+{
+    char a[] = "hello world";
+    char b[] = "hello world";
+    char *or = memset   (b + 2,'1',3);
+    char *ft = ft_memset(a + 2,'1',3);
+
+    printf("%s\n",a);
+    printf("%s\n",b);
+    printf("or %s\n",or);
+    printf("ft %s\n",ft);
+}
