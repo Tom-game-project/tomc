80996147c30286fd7b6f90b8e04fc7db88de5b8d
Tomonari Muranaka <tmuranak@c2r8s8.42tokyo.jp>
2024-04-25 15:53:08 +0900

    fix some function

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: tmuranak <tmuranak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 12:25:13 by tmuranak          #+#    #+#             */
-/*   Updated: 2024/04/20 12:38:08 by tmuranak         ###   ########.fr       */
+/*   Updated: 2024/04/25 15:17:26 by tmuranak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ char	*ft_strrchr(const char *s, int c)
 		count++;
 	if (!c)
 		return (strtmp + count);
-	while (count)
+	while (count + 1)
 	{
-		if (s[count] == c)
+		if ((unsigned char) s[count] == (unsigned char) c)
 			return (strtmp + count);
 		count--;
 	}
