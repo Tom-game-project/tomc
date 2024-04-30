825e9a8d79e64ac2d990d62812190c31a2e80dc3
Tomonari Muranaka <tmuranak@c2r3s10.42tokyo.jp>
2024-04-30 19:17:20 +0900

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: tmuranak <tmuranak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 19:11:07 by tmuranak          #+#    #+#             */
-/*   Updated: 2024/04/30 15:12:57 by tmuranak         ###   ########.fr       */
+/*   Updated: 2024/04/30 19:15:32 by tmuranak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	if (!s)
 		return (NULL);
-	buf = (char *)malloc((len + 1) * sizeof(char));
+	if (len < ft_strlen((char *)s))
+		buf = (char *)malloc((len + 1) * sizeof(char));
+	else
+		buf = (char *)malloc((ft_strlen((char *)s) + 1) * sizeof(char));
 	if (!buf)
 		return (NULL);
 	i = 0;
-	while (i < len && start < ft_strlen((char *)s))
+	while (i < len && start + i < ft_strlen((char *)s))
 	{
-		if (!s[start + i])
+		if (s[start + i] == '\0')
 		{
 			buf[i] = '\0';
 			return (buf);
